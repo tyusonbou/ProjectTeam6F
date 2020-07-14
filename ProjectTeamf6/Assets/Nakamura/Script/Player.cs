e375a408f4c8a2366d3d3dc37eb69ed92ebbf6de
KPC-017A2070\K017A2070 <tyu_sonbou@icloud.com>
2020-07-14 19:23:36 +0900

    フォルダ作成、Player作成
    自分の各フォルダ作成
    PlayerScriptを作成
    まだ移動のみ、正規化もまだ

---
+++ ProjectTeamf6/Assets/Nakamura/Script/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float WalkSped;
+    [SerializeField]
+    float RunSpeed;
+
+    string MoveState;
+
+    float LR;
+    float UD;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeState();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ChangeState()
+    {
+        LR = 0;
+        UD = 0;
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            LR = 1;
+            MoveState = "RIGHT";
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            LR = -1;
+            MoveState = "LEFT";
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            UD = 1;
+            MoveState = "UP";
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            UD = -1;
+            MoveState = "DOWN";
+        }
+    }
+
+    void Move()
+    {
+        transform.position += new Vector3(RunSpeed * Time.deltaTime * LR, RunSpeed * Time.deltaTime * UD, 0);
+    }
+}

7678d1d5d4f096713a5b89ddf99b574add6b07f7
shimada15 <iukfsafczn@i.softbank.jp>
2020-07-16 02:26:45 +0900

    拠点のテスト
    拠点（アタックバフ）のテスト

---
+++ ProjectTeamf6/Assets/Shimada/Scripts/Base.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+    Status status;
+    GameObject Player;
+    private float AttackPoint;
+    public bool ON;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("空 全体図");
+        status = Player.GetComponent<Status>();
+        ON = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ON)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                StartCoroutine("AttackUp");
+                ON = false;
+            }
+        }
+    }
+
+    IEnumerator AttackUp()
+    {
+        AttackPoint = status.Attack * 0.5f;
+        status.Attack = status.Attack + AttackPoint;
+        yield return null;
+    }
+}
---
+++ ProjectTeamf6/Assets/Shimada/Scripts/Status.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    public float HP;
+    public float Attack;
+    public float MaxHP = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

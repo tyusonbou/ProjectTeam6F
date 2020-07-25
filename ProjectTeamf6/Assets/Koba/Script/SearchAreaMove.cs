9d97c509b7c8a82dfc91319174a30dcc2976c82c
koyu12 <hwhw1211@gmail.com>
2020-07-26 04:19:39 +0900

    エネミーの処理追加

---
+++ ProjectTeamf6/Assets/Koba/Script/SearchAreaMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchAreaMove : MonoBehaviour
+{
+    public bool IsSearchPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsSearchPlayer = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsSearchPlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            IsSearchPlayer = false;
+        }
+    }
+
+    public bool RetrunIsSearchPlayer()
+    {
+        return IsSearchPlayer;
+    }
+}
+

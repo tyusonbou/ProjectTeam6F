1bc38cfab9a4a702dd2ca27fa386c55e62e9ddc5
koyu12 <hwhw1211@gmail.com>
2020-07-25 16:47:03 +0900

    エネミーの処理追加
    注意を引くオブジェクトの追加
    ゾンビの上限

---
+++ ProjectTeamf6/Assets/AttractScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractScript : MonoBehaviour
+{
+    [SerializeField, Header("注意を引く時間"), Range(0, 100)]
+    private float attractTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractTime -= Time.deltaTime;
+        if(attractTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}

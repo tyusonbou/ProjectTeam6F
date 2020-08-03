f18217d73d50d5b7b2d2ef3dd6f446c3168a5d4b
KPC-017A2070\K017A2070 <tyu_sonbou@icloud.com>
2020-08-03 11:37:16 +0900

    FPS固定、タイマー作成、プレイヤー修正
    QualityからFPS固定
    時間図るタイマーさくせい
    発煙筒使用回数設定

---
+++ ProjectTeamf6/Assets/Nakamura/Script/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    //　トータル制限時間
+    [SerializeField]
+    private float totalTime;
+    [SerializeField]
+    private int minute;
+    [SerializeField]
+    private float seconds;
+    //　前のUpdateの時の秒数
+    private float oldSeconds;
+
+    [SerializeField]
+    private Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalTime = minute * 60 + seconds;
+        timerText = GetComponentInChildren<Text>();
+        oldSeconds = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Approximately(Time.timeScale, 0f)) { return; }
+
+        //　制限時間が0秒以下なら何もしない
+        //if (totalTime <= 0f)
+        //{
+        //    return;
+        //}
+
+        //　一旦トータルの制限時間を計測；
+        totalTime = minute * 60 + seconds;
+        totalTime += Time.deltaTime;
+
+        //　再設定
+        minute = (int)totalTime / 60;
+        seconds = totalTime - minute * 60;
+
+        //　値が変わった時だけテキストUIを更新
+        if ((int)seconds != (int)oldSeconds)
+        {
+            timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+        }
+        oldSeconds = seconds;
+
+        ////　制限時間以下になったら死亡
+        //if (totalTime <= 0f)
+        //{
+        //    Destroy(GameObject.Find("Player"));
+        //}
+    }
+}
